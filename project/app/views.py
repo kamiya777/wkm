c77492fc240ec5f50a2e4c316ef7a2ff1682bb05
kamiya777 <68119588+kamiya777@users.noreply.github.com>
2020-07-21 22:24:43 +0900

    7/21

--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,resolve_url
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth. models import User
-from django. views.generic import DetailView
+from django. views.generic import DetailView,UpdateView,DeleteView,CreateView
+from .form import UserForm,ListForm
+from .mixins import OnlyYouMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from . models import List
+
 # Create your views here.
 
 def index(request):
@@ -17,7 +23,7 @@ def signup(request):
     if request.method == 'POST':
         form=UserCreationForm(request.POST)
         if form.is_valid():
-            use_instance=form.save()
+            user_instance=form.save()
             login(request,user_instance)
             return redirect("app:home")
     else:
@@ -32,6 +38,30 @@ def signup(request):
 def home(request):
     return render(request,"app/home.html")
 
-class UserDetailView(DetailView):
+class UserDetailView(LoginRequiredMixin,DetailView):
     model = User
     template_name="app/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin,UpdateView):
+    model=User
+    template_name="app/users/update.html"
+    form_class=UserForm
+
+    def get_success_url(self):
+        return resolve_url('app:users_detail',pk=self.kwargs['pk'])
+
+class UserDeleteView(OnlyYouMixin,DeleteView):
+    model=User
+    template_name="app/users/delete.html"
+
+    def get_success_url(self):
+        return resolve_url('app:users_detail',pk=self.kwargs['pk'])
+
+class ListCreateView(LoginRequiredMixin,CreateView):
+    model = List
+    template_name = "app/lists/create.html"
+    success_url = reverse_lazy("app:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
